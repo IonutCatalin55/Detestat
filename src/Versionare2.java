fe21af38f49f8699b00cbe9996e14d055bdad002
Ionut Catalin <ionutcatalin55@gmail.com>
2022-02-12 17:13:51 +0200

    test comit diferente

--- src/Versionare2.java
+++ src/Versionare2.java
@@ -1,6 +1,7 @@
 public class Versionare2 {
     public static void main(String[] args) {
         System.out.println("bagamias toate " + acum(2,24,25));
+        System.out.println("dea drac ");
     }
     public static int acum(int x, int y,int z){
         if(z> 22 & x > 22 & y > 22){
